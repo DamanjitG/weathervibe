856282e9d007d7a4556dc81d97c0f1349b128dd0
Daman Gill <1gilldam2@gmail.com>
2021-01-16 16:16:31 -0500

    Fixed uri callback

--- spotify-auth.js
+++ spotify-auth.js
@@ -22,7 +22,7 @@ window.authSpotify = function () {
 
   const authEndpoint = "https://accounts.spotify.com/authorize";
   const clientId = "2c059958c8a548a69d95f751753097b6";
-  const redirectUri = "http://127.0.0.1:5500/callback.html";
+  const redirectUri = "https://damanjitg.github.io/weathervibe/callback.html";
   const scopes = ["user-read-email"];
 
   // If there is no token, redirect to Spotify authorization
